9a5a415cf85f6b3a3f555d8f98f3ab76ba4ef260
nbiamino <nbiamino@gmail.com>
2015-06-10 18:04:26 +0200

    bug fix 3

--- src/main/java/fr/tse/fi2/hpp/labs/main/MainNonStreaming.java
+++ src/main/java/fr/tse/fi2/hpp/labs/main/MainNonStreaming.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+import org.bouncycastle.util.Integers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
